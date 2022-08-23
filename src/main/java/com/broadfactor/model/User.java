497c0ad744aa381fdd36a08cb867bb1e706622a1
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-08-23 20:52:20 -0300

    Criando entitade User e seu repository

---
+++ src/main/java/com/broadfactor/model/User.java
@@ -0,0 +1,46 @@
+package com.broadfactor.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1233734288022297122L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 562048007;
+    }
+}
