8f5dccc1aa414abfc5fd0e0b7775578e8e5c45e2
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-08-23 20:36:03 -0300

    initial commit

---
+++ src/main/java/com/broadfactor/BroadfactorApplication.java
@@ -0,0 +1,13 @@
+package com.broadfactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadfactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadfactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/broadfactor/BroadfactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broadfactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadfactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
