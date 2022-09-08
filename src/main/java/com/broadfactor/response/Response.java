0803b95ce64ea53457f59dabb980019766e4242f
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-07 22:36:10 -0300

    Serializando

--- src/main/java/com/broadfactor/response/Response.java
+++ src/main/java/com/broadfactor/response/Response.java
@@ -3,22 +3,26 @@ package com.broadfactor.response;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.validation.BindingResult;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public final class Response<Type> {
+public final class Response<Type> implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8374112162112593810L;
 
     private Type data;
     private List<String> errors;
 
     public List<String> getErrors() {
         if (errors == null) {
-            this.errors = new ArrayList<>();
+            errors = new ArrayList<>();
         }
         return errors;
     }
