166aedbf92965d51f727512dd5112ed7601092ac
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-09 21:40:20 -0300

    Implementando campos para ErrorResponse

---
+++ src/main/java/com/broadfactor/handler/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.broadfactor.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Data
+public class ErrorResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -6742734802738742010L;
+
+    private Integer status;
+    private String path;
+    private String message;
+    private String detail;
+
+}
