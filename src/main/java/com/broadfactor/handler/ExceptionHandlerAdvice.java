e06a9c041c6a6eaef740679e6b841cc255b0e02a
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-09 21:42:03 -0300

    Refatorando ExceptionHandlerAdvice

--- src/main/java/com/broadfactor/handler/ExceptionHandlerAdvice.java
+++ src/main/java/com/broadfactor/handler/ExceptionHandlerAdvice.java
@@ -8,16 +8,28 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<Response<Map<String, String>>> validationException(MethodArgumentNotValidException ex) {
-        Response<Map<String, String>> response = new Response<>();
+    public ResponseEntity<Response<ErrorResponse>> validationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        Response<ErrorResponse> response = new Response<>();
         ex.getBindingResult().getAllErrors().forEach(error -> response.setErrors(((FieldError) error).getField(), error.getDefaultMessage()));
+
+        Integer code = HttpStatus.BAD_REQUEST.value();
+        String path = request.getRequestURI();
+        String message = "Field Validation failed";
+        String detail = "Verifique os campos";
+
+        response.setData(new ErrorResponse(code, path, message, detail));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+
+//    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
+//    public ResponseEntity<Response<ErrorResponse>> sqlViolationException() {
+//
+//    }
 
 }
