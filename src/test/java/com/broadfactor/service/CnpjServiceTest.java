17191932a643f1b426f370098edc781e3d3af1d7
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-11 20:03:16 -0300

    Classe de teste para CnpjService

---
+++ src/test/java/com/broadfactor/service/CnpjServiceTest.java
@@ -0,0 +1,47 @@
+package com.broadfactor.service;
+
+import com.broadfactor.model.Cnpj;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
+public class CnpjServiceTest {
+
+    private static final String CNPJ = "13574594000196";
+    private static final String URL = "https://receitaws.com.br/v1/cnpj/";
+    private static final RestTemplate TEMPLATE = new RestTemplate();
+
+    @Autowired
+    private CnpjService service;
+
+    @Test
+    public void testValidatorCnpj() {
+        assertTrue(service.validatorCnpj(CNPJ));
+    }
+
+    @Test
+    public void testInvalidCnpj(){
+        assertFalse(service.validatorCnpj(""));
+        assertFalse(service.validatorCnpj(null));
+        assertFalse(service.validatorCnpj("12345"));
+    }
+
+    @Test
+    public void testFindCnpj() {
+        ResponseEntity<Cnpj> response = TEMPLATE.getForEntity(URL + CNPJ, Cnpj.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertInstanceOf(Cnpj.class, response.getBody());
+    }
+}
