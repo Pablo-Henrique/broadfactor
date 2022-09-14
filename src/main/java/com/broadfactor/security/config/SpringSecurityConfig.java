cda8b47a2762c74429d74a87cb9bb1886239dce8
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-14 11:40:58 -0300

    Bean de Configuração para autenticação

--- src/main/java/com/broadfactor/security/config/SpringSecurityConfig.java
+++ src/main/java/com/broadfactor/security/config/SpringSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
