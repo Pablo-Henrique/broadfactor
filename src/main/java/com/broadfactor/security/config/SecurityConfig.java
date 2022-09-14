3ef77ca6d79a8b7ae51453a4c11e83903a55875a
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-14 11:15:14 -0300

    COnfigurando Spring Security

---
+++ src/main/java/com/broadfactor/security/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.broadfactor.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .antMatcher("user/")
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+}
