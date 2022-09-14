58030227e21dd9a15772225d35c78a77a82315ac
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-14 11:56:31 -0300

    Refatorando ExceptionHandlerAdvice com novo ErrorResponse e adicionando SqlViolationException

--- src/main/java/com/broadfactor/handler/ExceptionHandlerAdvice.java
+++ src/main/java/com/broadfactor/handler/ExceptionHandlerAdvice.java
@@ -1,6 +1,8 @@
 package com.broadfactor.handler;
 
 import com.broadfactor.response.Response;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,27 +11,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<Response<ErrorResponse>> validationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+    public ResponseEntity<Response<ErrorResponse>> dtoValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
         Response<ErrorResponse> response = new Response<>();
         ex.getBindingResult().getAllErrors().forEach(error -> response.setErrors(((FieldError) error).getField(), error.getDefaultMessage()));
 
-        Integer code = HttpStatus.BAD_REQUEST.value();
-        String path = request.getRequestURI();
-        String message = "Field Validation failed";
-        String detail = "Verifique os campos";
+        ErrorResponse error = ErrorResponse
+                .builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(request.getRequestURI())
+                .message("Field Validation failed")
+                .detail("Verifique os campos")
+                .build();
 
-        response.setData(new ErrorResponse(code, path, message, detail));
+        response.setData(error);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
-//    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
-//    public ResponseEntity<Response<ErrorResponse>> sqlViolationException() {
-//
-//    }
+    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<Response<ErrorResponse>> sqlViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+        Response<ErrorResponse> response = new Response<>();
 
+        ErrorResponse error = ErrorResponse
+                .builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .path(request.getRequestURI())
+                .message("Dados já cadastrado em nossa base de dados!")
+                .detail("Verifique os campos")
+                .build();
+
+        response.setData(error);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
