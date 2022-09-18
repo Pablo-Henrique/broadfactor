bf7cb5fa64bd88c6fe19cd63903ce3f228e43074
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-18 15:04:53 -0300

    Anotação do SpringSecurity

--- src/main/java/com/broadfactor/security/config/SpringSecurityConfig.java
+++ src/main/java/com/broadfactor/security/config/SpringSecurityConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
