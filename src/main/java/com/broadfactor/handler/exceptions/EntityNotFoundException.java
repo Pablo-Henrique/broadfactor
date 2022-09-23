900d14c1b39b46881a9070a13ca6e5971b79c280
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-23 20:51:20 -0300

    Implementando Exception EntityNotFoundException

---
+++ src/main/java/com/broadfactor/handler/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.broadfactor.handler.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
