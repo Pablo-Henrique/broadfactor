7ec8618234c0a98a44c7de8f2eaa6ac40f9e8e78
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-09-27 10:21:19 -0300

    Criando exception ParseFormatterErrorException

---
+++ src/main/java/com/broadfactor/handler/exceptions/ParseFormatterErrorException.java
@@ -0,0 +1,8 @@
+package com.broadfactor.handler.exceptions;
+
+public class ParseFormatterErrorException extends RuntimeException {
+
+    public ParseFormatterErrorException(String message) {
+        super(message);
+    }
+}
