a46438efd4e78269ae7c64a41ee787a2f21a7e30
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-10-11 11:51:33 -0300

    Bean de config para Java Mail Sender

---
+++ src/main/java/com/broadfactor/config/JMSConfig.java
@@ -0,0 +1,15 @@
+package com.broadfactor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JMSConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
